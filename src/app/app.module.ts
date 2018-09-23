fd40503df10ce571dc1af51747d9ea72ccf2661a
Salsabeel Tolba <salsabeel.tolba@gmail.com>
2018-09-22 22:54:18 -0400

    added fullcalendar to be used for scheduling purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from 'ng-fullcalendar';
 
 import { AppComponent } from './app.component';
 import { MaterialDashboardComponent } from './material-dashboard/material-dashboard.component';
@@ -7,16 +8,19 @@ import { NoteToSelfComponent } from './note-to-self/note-to-self.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { CalendarComponent } from './calendar/calendar.component';
+import { FullCalendarComponent } from './full-calendar/full-calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaterialDashboardComponent,
     CalendarComponent,
-    NoteToSelfComponent
+    NoteToSelfComponent,
+    FullCalendarComponent
   ],
   imports: [
     BrowserModule,
+    FullCalendarModule,
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
---
+++ src/app/full-calendar/full-calendar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarComponent } from 'ng-fullcalendar';
+import { Options } from 'fullcalendar';
+
+@Component({
+  selector: 'app-full-calendar',
+  templateUrl: './full-calendar.component.html',
+  styleUrls: ['./full-calendar.component.css']
+})
+export class FullCalendarComponent implements OnInit {
+  calendarOptions: Options;
+  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
+  constructor() {}
+  ngOnInit() {
+    this.eventService.getEvents().subscribe(data => {
+      this.calendarOptions = {
+        editable: true,
+        eventLimit: false,
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay,listMonth'
+        },
+        selectable: true,
+        events: [],
+        };
+    });
+  }
+  clearEvents() {
+    this.events = [];
+  }
+  loadEvents() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
+  }
+  
