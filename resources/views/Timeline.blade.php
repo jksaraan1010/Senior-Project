a2095e5ebe014483184418ac7ce67cd211814a6f
Moosab Ahsan <moosab.ahsan@gmail.com>
2018-10-16 16:50:46 -0400

    added checkboxes and changed timeline layout

--- resources/views/Timeline.blade.php
+++ resources/views/Timeline.blade.php
@@ -11,111 +11,328 @@
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Start taking medication by yourself without reminders </p>
-            <p>Learn to feel comfortable taking care of yourself</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Learn about your medical conditions and medications </p>
-            <p>Understand what complications may come from your medical conditions</p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Understand that you will see an adult doctor in the next few years </p>
-            <p>Start to speak with the doctor by yourself without help from family or friends</p>
+            <label class="container">Start taking medication by yourself without reminders 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Learn to be comfortable taking care of yourself
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label> 
         </div>
-          
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>13-15</time>
           </div>
         </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Learn about your medical conditions and medications 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Understand what complications may come from your medical conditions
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+        </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Understand that you will see an adult doctor in the next few years 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Start to speak with the doctor by yourself without help from family or friends
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+        </div>
+
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Be able to take medication by yourself without any help </p>
-            <p>Feel comfortable taking care of yourself</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Know your complete medical history and medications </p>
-            <p>Know what to do and who to talk to when you feel ill  </p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Be able to talk to your doctors and nurses by yourself</p>
-            <p>Know how to call the doctor's office if you feel ill  </p>
+            <label class="container">Be able to take medication by yourself without any help 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Feel comfortable taking care of yourself
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
          </div>
-          
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>15-17</time>
           </div>
         </div>
+
+      <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Know your complete medical history and medications 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know what to do and who to talk to when you feel ill  
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+         </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Be able to talk to your doctors and nurses by yourself
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know how to call the doctor's office if you feel ill  
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+         </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Know how to get your medications refilled </p>
-            <p>Know how to arrange for transportation to your doctor's appointment</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Learn what type of insurance you have and how to apply for it  </p>
-            <p>Know which doctors you need to see and why you are seeing them</p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Feel prepared to transition from your current doctor to an adult doctor</p>
-            <p>Be able to schedule doctor's appointments by yourself</p>
+            <label class="container">Know how to get your medications refilled 
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know how to arrange for transportation to your doctor's appointment
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
           </div>
           
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>17-19</time>
+          </div>
+        </div>
+
+      <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Learn what type of insurance you have and how to apply for it  
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know which doctors you need to see and why you are seeing them
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Feel prepared to transition from your current doctor to an adult doctor
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Be able to schedule doctor's appointments by yourself
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
           </div>
         </div>
       </div>
     </div>
 
 <h1></h1>
+
+
 <div class="container">
       <div class="timeline">
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Start taking medication by yourself without reminders </p>
-            <p>Learn to feel comfortable taking care of yourself</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Learn about your medical conditions and medications </p>
-            <p>Understand what complications may come from your medical conditions</p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Understand that you will see an adult doctor in the next few years </p>
-            <p>Start to speak with the doctor by yourself without help from family or friends</p>
+            <label class="container">Start taking medication by yourself without reminders 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label> 
+            <label class="container">Learn to be comfortable taking care of yourself
+              <input type="checkbox">
+              <span class="checkmark"></span>
+            </label>
         </div>
-          
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>13-15</time>
           </div>
         </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Learn about your medical conditions and medications 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Understand what complications may come from your medical conditions
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+        </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Understand that you will see an adult doctor in the next few years 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Start to speak with the doctor by yourself without help from family or friends
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+        </div>
+
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Be able to take medication by yourself without any help </p>
-            <p>Feel comfortable taking care of yourself</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Know your complete medical history and medications </p>
-            <p>Know what to do and who to talk to when you feel ill  </p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Be able to talk to your doctors and nurses by yourself</p>
-            <p>Know how to call the doctor's office if you feel ill  </p>
+            <label class="container">Be able to take medication by yourself without any help 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Feel comfortable taking care of yourself
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
          </div>
-          
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>15-17</time>
           </div>
         </div>
+
+      <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Know your complete medical history and medications 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know what to do and who to talk to when you feel ill  
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+         </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Be able to talk to your doctors and nurses by yourself
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know how to call the doctor's office if you feel ill  
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+         </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
         <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
           <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
             <h3 class=" text-light">Self-Care</h3>
-            <p>Know how to get your medications refilled </p>
-            <p>Know how to arrange for transportation to your doctor's appointment</p>
-            <h3 class=" text-light">Health Awareness</h3>
-            <p>Learn what type of insurance you have and how to apply for it  </p>
-            <p>Know which doctors you need to see and why you are seeing them</p>
-            <h3 class=" text-light">Communication</h3>
-            <p>Feel prepared to transition from your current doctor to an adult doctor</p>
-            <p>Be able to schedule doctor's appointments by yourself</p>
+            <label class="container">Know how to get your medications refilled 
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know how to arrange for transportation to your doctor's appointment
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
           </div>
           
           <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
             <time>17-19</time>
           </div>
         </div>
+
+      <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Health Awareness</h3>
+            <label class="container">Learn what type of insurance you have and how to apply for it  
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Know which doctors you need to see and why you are seeing them
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Communication</h3>
+            <label class="container">Feel prepared to transition from your current doctor to an adult doctor
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+            <label class="container">Be able to schedule doctor's appointments by yourself
+              <input type="checkbox" checked="checked">
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time></time>
+          </div>
+        </div>
+
       </div>
     </div>
 @endsection
