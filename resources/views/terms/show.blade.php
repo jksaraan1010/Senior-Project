75bfa1f78b865b89fd34371f1250c66ae9d78968
Salsabeel Tolba <salsabeel.tolba@gmail.com>
2018-11-12 13:07:36 -0500

    terms and conditions page

--- resources/views/terms/show.blade.php
+++ resources/views/terms/show.blade.php
@@ -1,96 +1,96 @@
 <!DOCTYPE html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Terms</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('adminlte/css/font-awesome.min.css')}}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="{{asset('adminlte/css/ionicons.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('adminlte/css/adminlte.min.css')}}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('adminlte/css/blue.css')}}">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  
-  <style type="text/css">
-   .invalid-feedback {
-      display: block;
-    }
-  .terms_card{
-    padding: 20px!important;
-  }
-  .card-title {
-    margin-bottom: 1.75rem;
-        font-size: 20px;
-    font-weight: 900;
- /*   border-bottom: 1px solid black;*/
-  }
-  .terms_title{
-   border-bottom: 1px solid #E2E2E2;
-  }
-  .terms_description{
-    margin-top: 20px;
-  }
-</style>
-</head>
-<body class="hold-transition login-page">
-
-  <div class="login-logo">
-    <P> Terms and Condition</a>
-  </div>
-
-  <div class="card-body login-card-body">
-  <div class="container-fluid">
-    <div class="row">
-    <div class="col-md-12">
-        <!-- <h3 class="page-title">Terms and Condition</h3> -->
-        <div class="card terms_card">
-          <div class="card-block">
-            <div class="terms_title">
-              <h2 class="card-title">Terms</h2>
-            </div>
-            <div class="terms_description">
+<!-- saved from url=(0068)https://adminlte.io/themes/dev/AdminLTE/pages/examples/register.html -->
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Registration Page</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      <title>Log in</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      <link rel="stylesheet" href="{{ asset('Landing-Page-Template-Bootstrap-master/css/bootstrap.min.css') }}">
+      <!-- Material Design Bootstrap -->
+      <link rel="stylesheet" href="{{ asset('Landing-Page-Template-Bootstrap-master/css/mdb.min.css') }}">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="{{asset('adminlte/css/font-awesome.min.css')}}">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="{{asset('adminlte/css/ionicons.min.css')}}">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="{{asset('adminlte/css/adminlte.min.css')}}">
+      <!-- iCheck -->
+      <link rel="stylesheet" href="{{asset('adminlte/css/blue.css')}}">
+      <!-- Google Font: Source Sans Pro -->
+      <style type="text/css">
+         html, body {
+         height: 100%;
+         }
+         .flex-fill {
+         flex:1;
+         }
+         .mask {
+         position: absolute;
+         top: 0;
+         left: 0;
+         right: 0;
+         bottom: 0;
+         width: 100%;
+         height: 100%;
+         background-attachment: fixed; }
+         .invalid-feedback {
+         display: block;
+         }
+      </style>
+   </head>
+   <body class="hold-transition register-page">
+      <div style="background-image: url('/terms.jpg'); background-repeat: no-repeat; background-size: cover; height:100%;background-position: center center;">
+         <div class="mask d-flex flex-grow-1 align-items-center rgba-blue-strong">
+            <div class="register-box">
+            <div class="">
+                        <!-- landing url go below -->
+                        <h2 class="text-center text-white text-uppercase font-weight-bold mt-1 mb-2 pt-3">My Transition Explorer</h2>
+        </div>
+               <div class="card">
+                  <div class="card-body register-card-body">
+                     <p class="login-box-msg">Terms & Conditions</p>
+                     <div class="row">
+                     <div class="terms_description">
               @if(count($terms) > 0)
                 {!! $terms[0]->description !!}
               @endif
             </div>
-          </div>
         </div>
-    </div>
-    <div class="terms">
-    <a href="{{ url()->previous() }}" class="btn btn-xs btn-danger">Back</a>
-    </div>
-    </div>
-
-
-  </div>
-</div>
-
-
-
-
-
-<!-- jQuery -->
-<script src="{{asset('adminlte/js/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('adminlte/js/bootstrap.bundle.min.js')}}"></script>
-<!-- iCheck -->
-<script src="{{asset('adminlte/js/icheck.min.js')}}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass   : 'iradio_square-blue',
-      increaseArea : '20%' // optional
-    })
-  })
-</script>
-
-
-</body></html>
-
+                <div>        
+            <a href="{{ url()->previous() }}" class="btn btn-block btn-danger">Back</a>
+                  </div>
+                  <!-- /.form-box -->
+               </div>
+               <!-- /.card -->
+            </div>
+            <!-- /.register-box -->
+         </div>
+         </section>
+         <!-- jQuery -->
+         <script src="{{asset('adminlte/js/jquery.min.js')}}"></script>
+         <!-- Bootstrap 4 -->
+         <script src="{{asset('adminlte/js/bootstrap.bundle.min.js')}}"></script>
+         <!-- iCheck -->
+         <script src="{{asset('adminlte/js/icheck.min.js')}}"></script>
+         <script>
+            $(function () {
+              $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass   : 'iradio_square-blue',
+              increaseArea : '20%' // optional
+            })
+            })
+         </script>
+         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+      </div>
+      </div>
+   </body>
+</html>
